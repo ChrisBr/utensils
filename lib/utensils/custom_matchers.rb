2fc48075aa49286ef5e6b15d9ac95f94c2137263
Christian Bruckmayer <christian-bruckmayer@cookpad.com>
2020-03-03 08:20:50 +0100

    Remove data-original
    Obsolete and not needed anymore.

--- lib/utensils/custom_matchers.rb
+++ lib/utensils/custom_matchers.rb
@@ -94,8 +94,7 @@ module Utensils
         end
 
         def find_lazy_loaded_image
-          @page.has_css?("img[data-original$='#{@image}']") || 
-            @page.has_css?("img[data-src$='#{@image}']")
+          @page.has_css?("img[data-src$='#{@image}']")
         end
 
         def find_dragonfly_image
